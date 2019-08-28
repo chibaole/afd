663f513cde7d5b115cf354684f8921a6ecb76d7e
chibaole <pan.liu@chibaole.cc>
2019-08-28 17:31:34 +0800

    测试

--- src/router/index.js
+++ src/router/index.js
@@ -2,23 +2,23 @@
  * @Author: 刘攀
  * @Date: 2019-08-28 15:15:17
  * @LastEditors: 刘攀
- * @LastEditTime: 2019-08-28 17:28:22
+ * @LastEditTime: 2019-08-28 17:30:53
  * @Description: file content
  */
 import Vue from 'vue'
 import Router from 'vue-router'
-// import HelloWorld from '@/components/HelloWorld'
+import HelloWorld from '@/components/HelloWorld'
 import Home from '@/web/home'
 Vue.use(Router)
 
 export default new Router({
-  mode: 'history',
+  // mode: 'history',
   routes: [
-    // {
-    //   path: '/',
-    //   name: 'HelloWorld',
-    //   component: HelloWorld
-    // },
+    {
+      path: '/',
+      name: 'HelloWorld',
+      component: HelloWorld
+    },
     {
       path: '/home',
       name: 'home',
