c23b58796a8ef298083647fc4c3b29a4e433991c
chibaole <pan.liu@chibaole.cc>
2019-08-28 17:22:07 +0800

    首页标题

--- src/web/home.vue
+++ src/web/home.vue
@@ -2,7 +2,7 @@
  * @Author: 刘攀
  * @Date: 2019-08-28 15:57:26
  * @LastEditors: 刘攀
- * @LastEditTime: 2019-08-28 16:32:06
+ * @LastEditTime: 2019-08-28 17:21:41
  * @Description: file content
  -->
 <template>
@@ -15,7 +15,7 @@ export default {
   name: 'home',
   data () {
     return {
-      hometitle: '首页'
+      hometitle: '首页标题'
     }
   }
 }
